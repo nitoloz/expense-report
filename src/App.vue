ad3f53e636d0a87e15b909d4df73e447982e52cf
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-01-15 15:53:03 +0100

    feat:  add expenses categories list

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ExpenseTypesList/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ExpenseTypesList from './components/ExpenseTypesList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    ExpenseTypesList
   }
 }
 </script>
---
+++ src/components/ExpenseTypesList.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <div v-for="type in types">
+            {{ type.name }}
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'ExpenseTypesList',
+    props: {},
+    data() {
+      return {
+        types: [
+          {
+            name: 'Lebensmittel'
+          },
+          {
+            name: 'Restaurants'
+          },
+          {
+            name: 'Transport'
+          },
+          {
+            name: 'Reisen'
+          },
+          {
+            name: 'Haus'
+          },
+          {
+            name: 'Unterhaltung'
+          },
+          {
+            name: 'Shopping'
+          },
+          {
+            name: 'Shopping'
+          },
+          {
+            name: 'Lehre'
+          },
+          {
+            name: 'Medizin'
+          },
+          {
+            name: 'Haushaltswaren'
+          },
+          {
+            name: 'Sonstig'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    /*h3 {*/
+    /*margin: 40px 0 0;*/
+    /*}*/
+    /*ul {*/
+    /*list-style-type: none;*/
+    /*padding: 0;*/
+    /*}*/
+    /*li {*/
+    /*display: inline-block;*/
+    /*margin: 0 10px;*/
+    /*}*/
+    /*a {*/
+    /*color: #42b983;*/
+    /*}*/
+</style>
