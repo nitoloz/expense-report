a6f6309a55bd99134dc274d45d4e049c04c024b5
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-01-17 13:17:22 +0100

    feat: add file upload component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app" class="container">
     <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <ExpenseReportUpload/>
     <ExpenseItem item="REWE Frankfurt/Mai / Frankfurt"/>
     <ExpenseTypesList/>
   </div>
@@ -9,12 +10,14 @@
 <script>
 import ExpenseItem from './components/ExpenseItem.vue'
 import ExpenseTypesList from './components/ExpenseTypesList.vue'
+import ExpenseReportUpload from './components/ExpenseReportUpload.vue'
 
 export default {
   name: 'app',
   components: {
     ExpenseItem,
-    ExpenseTypesList
+    ExpenseTypesList,
+    ExpenseReportUpload
   }
 }
 </script>
---
+++ src/components/ExpenseReportUpload.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button type="button" class="btn btn-primary">Select file</button>
+        <button type="button" class="btn btn-secondary"> Upload file</button>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'ExpenseReportUpload',
+    props: {
+      item: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
