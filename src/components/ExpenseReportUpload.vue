5b7aa9179c38704b0791d97c3f37a6a1c32d0ff9
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-01-19 13:27:43 +0100

    feat: improve data parser

--- src/components/ExpenseReportUpload.vue
+++ src/components/ExpenseReportUpload.vue
@@ -10,37 +10,41 @@
 
 <script>
 
-  export default {
-    name: 'ExpenseReportUpload',
-    props: {
-      item: String
-    },
-    methods: {
-      selectFileClick: function () {
-        this.$refs.fileInput.click();
-      },
-      fileChanged: function () {
-        console.log(this.$refs.fileInput.files);
-        let reader = new FileReader();
-        reader.onload = function(){
-          let expenses = [];
-          let data = reader.result.split(/\r\n|\n/).map(line => line.split(','));
-          for(let i = 1;i<data.length;i++){
-            let expense = {};
-            data[i].forEach((item, index) => expense[data[0][index].trim()] = item.trim());
-            expenses.push(expense);
-          }
-          console.log(expenses);
-        };
-        reader.readAsText(this.$refs.fileInput.files[0]);
-      }
-    },
-    data() {
-      return {
-        file: null
-      }
+    export default {
+        name: 'ExpenseReportUpload',
+        props: {
+            item: String
+        },
+        methods: {
+            selectFileClick: function () {
+                this.$refs.fileInput.click();
+            },
+            fileChanged: function () {
+                console.log(this.$refs.fileInput.files);
+                let reader = new FileReader();
+                reader.onload = function () {
+                    let allTransactions = [];
+                    let spendings = [];
+                    let data = reader.result.split(/\r\n|\n/).map(line => line.split(';'));
+                    const amountColumnLabel = data[0][data[0].length - 1];
+                    for (let i = 1; i < data.length; i++) {
+                        let expense = {};
+                        data[i].forEach((item, index) => expense[data[0][index].trim()] = item.trim());
+                        expense[amountColumnLabel] = expense[amountColumnLabel] ? parseInt(expense[amountColumnLabel]) : 0;
+                        allTransactions.push(expense);
+                    }
+                    spendings = allTransactions.filter(transaction => transaction[amountColumnLabel] < 0);
+                    console.log(spendings);
+                };
+                reader.readAsText(this.$refs.fileInput.files[0]);
+            }
+        },
+        data() {
+            return {
+                file: null
+            }
+        }
     }
-  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
