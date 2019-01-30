df9247caefaa3d5bd09e248f9e89e6170b5c6037
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-01-30 12:33:19 +0100

    feat: add class for data processor

---
+++ src/services/DataProcessor.js
@@ -0,0 +1,36 @@
+import * as d3 from 'd3';
+
+const OTHERS_GROUP = 'Other';
+const AMOUNT = 'Betrag in EUR';
+
+export default class DataProcessor {
+
+  processPieChartData(data, groupByOption, minimalLevel = 0.01) {
+    const internalMap = new Map();
+    data.forEach(d => internalMap[d[groupByOption]]
+        ? internalMap[d[groupByOption]] += d[AMOUNT]
+        : internalMap[d[groupByOption]] = d[AMOUNT]);
+
+    const totalSum = d3.sum(data.map(d => d[AMOUNT]));
+
+    const defaultSortingFunction = (a, b) => {
+      return b.key === OTHERS_GROUP
+          ? -1
+          : a.key === OTHERS_GROUP
+              ? Number.MAX_VALUE
+              : b.value - a.value
+    };
+
+    return d3.nest()
+        .key(d => internalMap[d[groupByOption]] / totalSum > minimalLevel ? d[groupByOption] : OTHERS_GROUP)
+        .rollup(d => {
+          const values = d.map(response => response[AMOUNT]);
+          return {
+            value: -1 * d3.sum(values),
+            percentageValue: Math.round(d3.sum(values) / totalSum * 100),
+          }
+        })
+        .entries(data)
+        .sort(defaultSortingFunction);
+  }
+}
