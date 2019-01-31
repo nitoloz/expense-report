c9586a0ad5d3b6c7ed9509d45d80e3549c09e6c6
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-01-31 14:35:34 +0100

    feat: fit expense item in 1 line

--- src/components/ExpenseItem.vue
+++ src/components/ExpenseItem.vue
@@ -2,10 +2,10 @@
     <div>
         <h2 class="row">
             <div class="col-sm-1"><i class="fas fa-chevron-left mr-5" v-on:click="$emit('click-left')"></i></div>
-            <div class="col-sm-10">
-                {{ item['Umsatz/Ort'] }},
-                {{ item['Kaufdatum'] }},
-                {{ item['Betrag in EUR']}} EUR
+            <div class="col-sm-10 expense-item-property">
+                <div class="width-60">{{ item[ExpenseItem.EXPENSE_NAME] }},</div>
+                <div class="width-20">{{ item[ExpenseItem.PURCHASE_DATE] }},</div>
+                <div class="width-20">{{ item[ExpenseItem.PURCHASE_AMOUNT]}} EUR</div>
             </div>
             <div class="col-sm-1"><i class="fas fa-chevron-right ml-5" v-on:click="$emit('click-right')"></i></div>
         </h2>
@@ -13,12 +13,19 @@
 </template>
 
 <script>
-    export default {
-        name: 'ExpenseItem',
-        props: {
-            item: Object
-        }
+  import ExpenseItem from '../enums/ExpenseItem.js';
+
+  export default {
+    name: 'ExpenseItem',
+    props: {
+      item: Object
+    },
+    data() {
+      return {
+        ExpenseItem: ExpenseItem
+      }
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -26,4 +33,20 @@
     i {
         cursor: pointer;
     }
+
+    .expense-item-property div {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        float: left;
+    }
+
+    .width-60 {
+        width: 60%
+    }
+
+    .width-20 {
+        width: 20%
+    }
+
 </style>
---
+++ src/enums/ExpenseItem.js
@@ -0,0 +1,5 @@
+export default class ExpenseItem {
+  static EXPENSE_NAME = 'Umsatz/Ort';
+  static PURCHASE_DATE = 'Kaufdatum';
+  static PURCHASE_AMOUNT = 'Betrag in EUR';
+}
