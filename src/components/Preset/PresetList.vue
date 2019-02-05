e537442a5849a40255d33a183cac6a6536bae07e
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-02-05 12:56:53 +0100

    feat: remove preset expense

--- src/components/Preset/PresetList.vue
+++ src/components/Preset/PresetList.vue
@@ -9,6 +9,7 @@
                         <th scope="col">{{ExpenseItem.EXPENSE_NAME}}</th>
                         <th scope="col">{{ExpenseItem.PURCHASE_AMOUNT_SHORT}}</th>
                         <th scope="col">{{ExpenseItem.EXPENSE_TYPE_LABEL}}</th>
+                        <th scope="col">Action</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -17,6 +18,10 @@
                         <td>{{presetExpense[ExpenseItem.EXPENSE_NAME]}}</td>
                         <td>{{presetExpense[ExpenseItem.PURCHASE_AMOUNT]}} EUR</td>
                         <td>{{presetExpense[ExpenseItem.EXPENSE_TYPE]}}</td>
+                        <td class="cursor-pointer">
+                            <i class="fas fa-edit" @click="editExpense(presetExpense.id)"></i>
+                            <i class="fas fa-trash-alt ml-2" @click="removeExpense(presetExpense.id)"></i>
+                        </td>
                     </tr>
                     </tbody>
                 </table>
@@ -27,25 +32,33 @@
 
 <script>
 
-    import {db} from '../../main'
-    import ExpenseItem from '../../enums/ExpenseItem';
+  import {db} from '../../main'
+  import ExpenseItem from '../../enums/ExpenseItem';
 
-    export default {
-        name: 'PresetList',
-        props: {presetExpenses: Array},
-        components: {},
-        methods: {
-        },
-        data() {
-            return {
-                expenses: [],
-                ExpenseItem: ExpenseItem
-            }
-        }
+  export default {
+    name: 'PresetList',
+    props: {presetExpenses: Array},
+    components: {},
+    methods: {
+      removeExpense: function (id) {
+        db.collection('presetExpenses').doc(id).delete();
+      },
+      editExpense: function (id) {
+        console.log(`Editing ${id}`);
+      }
+    },
+    data() {
+      return {
+        expenses: [],
+        ExpenseItem: ExpenseItem
+      }
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+    .cursor-pointer i {
+        cursor: pointer;
+    }
 </style>
