80a3ec3ca291e0677aefe5b5da38776061bad86f
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-02-05 13:00:30 +0100

    feat: add preset component

---
+++ src/components/Preset/CreatePreset.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                Create new
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+  import {db} from '../../main'
+  import ExpenseItem from '../../enums/ExpenseItem';
+
+  export default {
+    name: 'CreatePreset',
+    props: {},
+    components: {},
+    methods: {
+      addExpense: function (expense) {
+        db.collection('presetExpenses').doc(id).delete();
+      }
+    },
+    data() {
+      return {
+        ExpenseItem: ExpenseItem
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/components/Preset/Preset.vue
+++ src/components/Preset/Preset.vue
@@ -2,6 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
+                <CreatePreset/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
                 <PresetList :presetExpenses="presetExpenses"/>
             </div>
         </div>
@@ -10,41 +15,42 @@
 
 <script>
 
-    import {db} from '../../main'
-    import ExpenseItem from '../../enums/ExpenseItem';
-    import PresetList from './PresetList';
+  import {db} from '../../main'
+  import ExpenseItem from '../../enums/ExpenseItem';
+  import PresetList from './PresetList';
+  import CreatePreset from './CreatePreset';
 
-    export default {
-        name: 'Preset',
-        props: {selectedMonth: String},
-        components: {PresetList},
-        // watch: {
-        //     selectedMonth: function () {
-        //         this.monthSelected();
-        //     }
-        // },
-        methods: {
-            // monthSelected: function () {
-            //     this.$bind('expenses', db.collection('expenses').doc(this.selectedMonth).collection('data'));
-            // }
-        },
-        data() {
-            return {
-                presetExpenses: [],
-                ExpenseItem: ExpenseItem
-            }
-        },
-        mounted() {
-            // if (this.selectedMonth) {
-            //     this.monthSelected();
-            // }
-        },
-        firestore() {
-            return {
-                presetExpenses: db.collection('presetExpenses').orderBy(ExpenseItem.PURCHASE_AMOUNT, "asc")
-            }
-        }
+  export default {
+    name: 'Preset',
+    props: {selectedMonth: String},
+    components: {PresetList, CreatePreset},
+    // watch: {
+    //     selectedMonth: function () {
+    //         this.monthSelected();
+    //     }
+    // },
+    methods: {
+      // monthSelected: function () {
+      //     this.$bind('expenses', db.collection('expenses').doc(this.selectedMonth).collection('data'));
+      // }
+    },
+    data() {
+      return {
+        presetExpenses: [],
+        ExpenseItem: ExpenseItem
+      }
+    },
+    mounted() {
+      // if (this.selectedMonth) {
+      //     this.monthSelected();
+      // }
+    },
+    firestore() {
+      return {
+        presetExpenses: db.collection('presetExpenses').orderBy(ExpenseItem.PURCHASE_AMOUNT, "asc")
+      }
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
