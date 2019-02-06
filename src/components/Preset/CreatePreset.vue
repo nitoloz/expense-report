c52882791b2d470ceda902b26c56af601c226e0e
nitoloz <andrey.zolotin@gmail.com>
2019-02-06 18:39:55 +0100

    feat: create preset

--- src/components/Preset/CreatePreset.vue
+++ src/components/Preset/CreatePreset.vue
@@ -1,36 +1,60 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                Create new
+        <form>
+            <div class="form-row text-left">
+                <div class="form-group col-sm-5">
+                    <label for="inputName">Name</label>
+                    <input class="form-control" v-model="newExpense[ExpenseItem.EXPENSE_NAME]" id="inputName"
+                           placeholder="Expense name">
+                </div>
+                <div class="form-group col-sm-2">
+                    <label for="inputBetrag">Betrag</label>
+                    <input type="number" v-model="newExpense[ExpenseItem.PURCHASE_AMOUNT]" class="form-control"
+                           id="inputBetrag" placeholder="Betrag in EUR">
+                </div>
+                <div class="form-group col-sm-3">
+                    <label for="inputBetrag">Type</label>
+                    <select class="form-control" v-model="newExpense[ExpenseItem.EXPENSE_TYPE]">
+                        <option selected disabled value="">Select type</option>
+                        <option v-for="type in expenseTypesArray" :key="type" v-bind:value="type">{{type}}</option>
+                    </select>
+                </div>
+                <div class="col-sm-2">
+                    <button type="submit" class="btn btn-primary mt-32" @click="addExpense">Add expense</button>
+                </div>
             </div>
-        </div>
+        </form>
     </div>
 </template>
 
 <script>
 
-  import {db} from '../../main'
-  import ExpenseItem from '../../enums/ExpenseItem';
+    import {db} from '../../main'
+    import ExpenseItem from '../../enums/ExpenseItem';
+    import {expenseTypesArray} from '../../enums/ExpenseTypes';
 
-  export default {
-    name: 'CreatePreset',
-    props: {},
-    components: {},
-    methods: {
-      addExpense: function (expense) {
-        db.collection('presetExpenses').doc(id).delete();
-      }
-    },
-    data() {
-      return {
-        ExpenseItem: ExpenseItem
-      }
+    export default {
+        name: 'CreatePreset',
+        props: {},
+        components: {},
+        methods: {
+            addExpense: function () {
+                db.collection('presetExpenses').add(this.newExpense);
+            }
+        },
+        data() {
+            return {
+                ExpenseItem: ExpenseItem,
+                expenseTypesArray: expenseTypesArray,
+                newExpense: {[ExpenseItem.EXPENSE_TYPE]: ''}
+            }
+        }
     }
-  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+    .mt-32 {
+        margin-top: 32px
+    }
 </style>
--- src/enums/ExpenseTypes.js
+++ src/enums/ExpenseTypes.js
@@ -1,14 +1,19 @@
-export default class ExpenseTypes{
-  static LEBENSMITTEL = 'Lebensmittel';
-  static RESTAURANTS = 'Restaurants';
-  static TRANSPORT = 'Transport';
-  static REISEN = 'Reisen';
-  static HAUS = 'Haus';
-  static UNTERHALTUNG = 'Unterhaltung';
-  static SHOPPING = 'Shopping';
-  static LEHRE = 'Lehre';
-  static MEDIZIN = 'Medizin';
-  static HAUSHALTSWAREN = 'Haushaltswaren';
-  static SONSTIG = 'Sonstig';
-  static OTHER = 'Other';
+export default class ExpenseTypes {
+    static LEBENSMITTEL = 'Lebensmittel';
+    static RESTAURANTS = 'Restaurants';
+    static TRANSPORT = 'Transport';
+    static REISEN = 'Reisen';
+    static HAUS = 'Haus';
+    static UNTERHALTUNG = 'Unterhaltung';
+    static SHOPPING = 'Shopping';
+    static LEHRE = 'Lehre';
+    static MEDIZIN = 'Medizin';
+    static HAUSHALTSWAREN = 'Haushaltswaren';
+    static SONSTIG = 'Sonstig';
+    static OTHER = 'Other';
 }
+
+export const expenseTypesArray = [
+    'Lebensmittel', 'Restaurants', 'Transport', 'Reisen', 'Haus', 'Unterhaltung',
+    'Shopping', 'Lehre', 'Medizin', 'Haushaltswaren', 'Sonstig', 'Other'
+];
