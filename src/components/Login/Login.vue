98b978b9609716e66e1747fe27944ac1bbad3e11
Andrey Zolotin <andrey.zolotin@gmail.com>
2019-02-07 15:49:19 +0100

    feat: create login form

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,8 +1,25 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-sm-12">
-                Login
+            <div class="col-sm-4 offset-sm-4 text-left">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <h3 class="text-center">Sign In</h3>
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Email address">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="formGroupExampleInput2"
+                                       placeholder="Password">
+                            </div>
+                            <button type="button" class="btn btn-primary btn-block">Sign in</button>
+                        </form>
+                        <hr/>
+                        <button type="button" class="btn btn-danger btn-block">Sign in with Google</button>
+                        <button type="button" class="btn btn-dark btn-block">Sign in with Github</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
