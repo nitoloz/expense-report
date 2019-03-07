fcd55ebed46161d76481955c8cb9d0d049f8e414
nitoloz <andrey.zolotin@gmail.com>
2019-03-07 22:54:08 +0100

    feat: change views location

--- src/components/Diagrams/Diagrams.vue
+++ src/components/Diagrams/Diagrams.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="container-fluid">
-        <h3 v-if="!selectedMonth"> Please select expenses month to view diagram!</h3>
+        <div class="row">
+            <div class="col-lg-12">
+                <BarChart :data="allExpenses"/>
+            </div>
+        </div>
+        <h3 v-if="!selectedMonth"> Please select expenses month to view monthly diagram!</h3>
         <div class="row">
             <div class="col-lg-7">
                 <PieChart :data="monthlyExpenses" :month="selectedMonth" v-on:select-pie-section="selectPieChartSection"/>
@@ -9,12 +14,6 @@
                 <ExpenseList :expensesSection="expensesSection"/>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <BarChart :data="allExpenses"/>
-            </div>
-        </div>
-
     </div>
 </template>
 
